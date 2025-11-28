4d0a55aa9ac98d14d5aa628c99ab28fda763823a
Glucio <glucio@dnp.gov.co>
2025-11-28 14:56:11 -0500

    Deploy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Publicaciones } from './pages/publicaciones/publicaciones';
 import { Home } from './pages/home/home';
 import { Semilleros } from './pages/semilleros/semilleros';
 import { Investigadores } from './pages/investigadores/investigadores';
+import { Eventos } from './pages/eventos/eventos';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -10,10 +11,11 @@ export const routes: Routes = [
   { path: 'semilleros', component: Semilleros },
   { path: 'investigadores', component: Investigadores },
   { path: 'publicaciones', component: Publicaciones },
+  { path: 'eventos', component: Publicaciones },
 
   // Ruta de detalle (CRÍTICA para Google Scholar)
   // Al entrar aquí, el servidor generará el meta-tag específico del artículo
-  { path: 'publicaciones/:id', component: Publicaciones },
+  { path: 'publicaciones/:id', component: Eventos },
 
   // Redirección inicial
   //{ path: '**', redirectTo: '/home' }
